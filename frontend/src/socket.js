00c24940eebec3088afe2e2b0d7c59b903f4d832
Ragavika <ragavikathumma@gmail.com>
2026-07-03 16:08:23 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // Connect to your backend server
-const socket = io("http://localhost:3000", {
+const socket = io("https://real-time-chat-application-7o0s.onrender.com", {
   transports: ["websocket"], // ensures WebSocket is used
   reconnection: true,        // auto-reconnect if connection drops
 });
