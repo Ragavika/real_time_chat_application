1514b61a5ad679f7902d6edefb5c19bcb931f43b
Ragavika <ragavikathumma@gmail.com>
2026-07-03 16:10:30 +0530

    Update CORS settings and server log message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "http://localhost:5173",
+const io = new Server(server, { cors: { origin: "*",
     methods: ["GET", "POST"] } });
 
 io.on("connection", (socket) => {
@@ -34,4 +34,4 @@ io.on("connection", (socket) => {
 });
 
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+server.listen(3000, () => console.log("Server running on ${PORT}"));
